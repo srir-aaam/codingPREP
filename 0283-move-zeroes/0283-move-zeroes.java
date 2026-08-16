32c36452a5a7a5a5b86468f66729f4f0602ff3e2
srir-aaam <119839493+srir-aaam@users.noreply.github.com>
2026-08-16 12:26:42 +0530

    Time: 2 ms (91.98%), Space: 47.6 MB (88.82%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }
+        }
+        if(j == -1)
+            return;
+        for(int i = j+1; i < nums.length; i++){
+            if(nums[i] != 0){
+                swap(nums, i ,j);
+                j++;
+            }
+        }
+
+    }
+    public void swap(int[] nums, int i , int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
